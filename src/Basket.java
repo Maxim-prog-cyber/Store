7555dc63e2b95f585661be2dd2262f1e5bc3d767
Maxim-prog-cyber <maxim.kuryzin@yandex.ru>
2020-11-21 22:06:50 +0300

    Initial commit

---
+++ src/Basket.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Basket {
+    ArrayList <String> abstractProduct = new ArrayList<>();
+    private int sumBasket;
+    private int numberOrder;
+
+    public ArrayList<String> getAbstractProduct() {
+        return abstractProduct;
+    }
+
+    public void setAbstractProduct(ArrayList<String> abstractProduct) {
+        this.abstractProduct = abstractProduct;
+    }
+
+    public int getSumBasket() {
+        return sumBasket;
+    }
+
+    public void setSumBasket(int sumBasket) {
+        this.sumBasket = sumBasket;
+    }
+
+    public int getNumberOrder() {
+        return numberOrder;
+    }
+
+    public void setNumberOrder(int numberOrder) {
+        this.numberOrder = numberOrder;
+    }
+
+    public void addProduct  (String abstractProduct){ /////Мой метод "Добавить товар"
+
+
+
+    }
+
+}
